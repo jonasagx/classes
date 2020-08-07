698a3e93416d711d5e13d4d81276a4826f30ef74
Jonas Galvão Xavier <jonas.agx@gmail.com>
2020-08-07 10:47:41 -0700

    qtd de cada moeda

--- caixa-guloso.py
+++ caixa-guloso.py
@@ -3,18 +3,18 @@
 # Change owed: 0.41
 # 4
 
-def decimals(value):
-     before, after = str(value).split(".")
-     if len(after) == 0:
-         return 1
-     return 10**len(after)
+# def decimals(value):
+#      before, after = str(value).split(".")
+#      if len(after) == 0:
+#          return 1
+#      return 10**len(after)
      
 while True:
     value = input("Value: ")
     if value <= 0:
         continue
 
-    value = int(value * decimals(value))
+    value = int(value)
     print(value)
     
     coin = [25, 10, 5, 1]
@@ -23,11 +23,12 @@ while True:
     change = []
 
     for c in coin:
-        while value % c != value:
-            nUniqueCoins = nUniqueCoins + 1
-            nCoins = nCoins + value/c
-            value = value % c
-            change.append(c)
+         while value % c != value:
+              # print(value, c, value/c)
+              nUniqueCoins = nUniqueCoins + 1
+              nCoins = nCoins + value/c
+              change.append((c, value/c))
+              value = value % c
 
     print(nUniqueCoins, nCoins, change)
     break
